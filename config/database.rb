6491aef2fe9b81ceb6d2118d2466b08d5369e135
Joshua Hawkins <jrchawkins@hotmail.com>
2018-06-28 23:33:47 +0000

    Readies for deployment

--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,7 @@ configure do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
-    set : database, {
+    set :database, {
     adapter: "sqlite3",
     database: "db/db/sqlite3"
     }
